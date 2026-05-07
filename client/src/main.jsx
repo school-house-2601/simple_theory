13ccdd559fa6673592815cb8c954c1dd2972119c
Sarah Hopp <sarahhopp717@gmail.com>
2026-05-06 19:46:02 -0700

    app fixes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./app";
+import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./features/05-Auth/AuthContext.jsx";
 
