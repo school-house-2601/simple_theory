0126ff97a5820ff6aafa666195ab41f712d45fe0
Sarah Hopp <sarahhopp717@gmail.com>
2026-06-01 19:12:51 -0700

    selection bug

--- client/src/features/02-Selection/SelectionPage.jsx
+++ client/src/features/02-Selection/SelectionPage.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../05-Auth/AuthContext";
 import PathCard from "./PathCard";
-import "./selection.css";
+import "./Selection.css";
 
 const PATHS = [
   {
