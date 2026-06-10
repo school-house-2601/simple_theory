fbe109970fc926f995451c3ba810f2450a2d2978
Sarah Hopp <sarahhopp717@gmail.com>
2026-06-10 16:57:28 -0700

    typo please work

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ import PracticePage from "./features/06-Practice/PracticePage.jsx";
 import SettingsPage from "./features/06-Settings/SettingsPage.jsx";
 import HowItWorksPage from "./shared/components/HowItWorks.jsx";
 import ProfilePage from "./features/07-Profile/ProfilePage.jsx";
-import ProtectedRoute from "./shared/componentes/ProtectedRoute.jsx";
+import ProtectedRoute from "./shared/components/ProtectedRoute.jsx";
 
 export default function App() {
   return (
