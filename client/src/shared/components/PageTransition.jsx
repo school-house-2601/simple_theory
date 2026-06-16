9570788b0c08806bab3ad10dc2f6baf0efafe59c
Joseph-Pena <penajoseph270@gmail.com>
2026-06-15 22:53:28 -0400

    animation

---
+++ client/src/shared/components/PageTransition.jsx
@@ -0,0 +1,14 @@
+import { motion } from "framer-motion";
+
+export default function PageTransition({ children }) {
+  return (
+    <motion.div
+      initial={{ opacity: 0, scale: 0.95 }}
+      animate={{ opacity: 1, scale: 1 }}
+      exit={{ opacity: 0, scale: 1.05 }}
+      transition={{ duration: 0.25 }}
+    >
+      {children}
+    </motion.div>
+  );
+}
